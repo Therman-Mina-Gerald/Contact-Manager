ae193f83bf87e40ee738abc492ee052d8588378d
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-10 16:06:33 -0600

    first step to read all lines

--- src/contactManagerApp.java
+++ src/contactManagerApp.java
@@ -5,10 +5,27 @@ import java.io.*;
 
 
 public class contactManagerApp {
+    Scanner scanner = new Scanner(System.in);
+   // String contactFile = "contacts.txt";
 
-    public static void main(String[] args) {
-
+    public static void printContact(Path filePath)throws IOException {
         System.out.println();
+        List<String> fileContents = Files.readAllLines(filePath);
+        //We need List as .readAllLines returns List datatype
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Path pathToContact = Paths.get("src/contacts.txt");
+        printContact(pathToContact);
+
+
+
 
     }
 }
